bc5c805398c976ac3204bbb94458924ae7de98ab
takkyuuplayer <takkyuuplayer@gmail.com>
2013-08-20 13:34:33 +0900

    Delete xenv

--- install.bash
+++ install.bash
@@ -10,9 +10,9 @@ git submodule update
 perl ./copies.pl
 
 # xenv
-git clone git://github.com/tokuhirom/plenv.git ~/.plenv/
-git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
-git clone git://github.com/sstephenson/rbenv.git ~/.rbenv/
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build/
-git clone git://github.com/yyuu/pyenv.git ~/.pyenv/
-git clone git://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/virtualenv/
+#git clone git://github.com/tokuhirom/plenv.git ~/.plenv/
+#git clone git://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
+#git clone git://github.com/sstephenson/rbenv.git ~/.rbenv/
+#git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build/
+#git clone git://github.com/yyuu/pyenv.git ~/.pyenv/
+#git clone git://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/virtualenv/
