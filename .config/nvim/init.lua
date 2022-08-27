abe1a54516ff6fc1fa1bd050ec153e1fed8e9be3
takkyuuplayer <takkyuuplayer@gmail.com>
2022-08-27 23:17:54 +0900

    auto fumt

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -83,12 +83,12 @@ vim.cmd('filetype plugin indent on')
 require('mason').setup()
 require('mason-lspconfig').setup_handlers({ function(server)
   local opt = {
-    -- -- Function executed when the LSP server startup
-    -- on_attach = function(client, bufnr)
-    --   local opts = { noremap=true, silent=true }
-    --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
-    -- end,
+    -- Function executed when the LSP server startup
+    on_attach = function(client, bufnr)
+      local opts = { noremap=true, silent=true }
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+    end,
     capabilities = require('cmp_nvim_lsp').update_capabilities(
       vim.lsp.protocol.make_client_capabilities()
     )
@@ -167,4 +167,3 @@ cmp.setup.cmdline(":", {
 })
 
 -- 4. my own
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
