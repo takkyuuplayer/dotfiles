09e703f99d4fb4a01cb554060eb9b5a09d8821ba
takkyuuplayer <takkyuuplayer@gmail.com>
2022-12-10 00:50:00 +0900

    update nvim setting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -85,13 +85,11 @@ require('mason-lspconfig').setup_handlers({ function(server)
   local opt = {
     -- Function executed when the LSP server startup
     on_attach = function(client, bufnr)
-      local opts = { noremap=true, silent=true }
+      local opts = { noremap = true, silent = true }
       vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
       vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
     end,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(
-      vim.lsp.protocol.make_client_capabilities()
-    )
+    capabilities = require('cmp_nvim_lsp').default_capabilities()
   }
   require('lspconfig')[server].setup(opt)
 end })
