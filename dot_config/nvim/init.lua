0173e6131bf79a12a312b876f0d63c60a63907d5
takkyuuplayer <takkyuuplayer@gmail.com>
2025-08-31 14:40:48 +0900

    add lua setting

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -320,4 +320,26 @@ cmp.setup.cmdline(":", {
   },
 })
 
+lspconfig.lua_ls.setup({
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = {'vim'},
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
 -- 4. my own
